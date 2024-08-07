1b8ea745e63f6662cf42efb8eda185aafecdeead
Roy Simanovich <fxp0kill@gmail.com>
2024-08-07 13:10:03 +0000

    Add files

---
+++ app/index.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-1' });
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: AWS_REGION });
+
+const port = 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/user/addUser', async(req, res) => {
+
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).send('Name and Email are required');
+    }
+
+    // Check if user already exists
+    const params = {
+        TableName: 'Users',
+        Key: {
+            username: name,
+        }
+    };
+
+    try
+    {
+        const data = await dynamoDB.get(params).promise();
+
+        if (data.Item) {
+            return res.status(400).send('User already exists');
+        }
+
+        // The user does not exist, add the user, need to be a unique ID
+        const params = {
+            TableName: 'Users',
+            Item: {
+                username: name,
+                email: email,
+            }
+        };
+
+        await dynamoDB.put(params).promise();
+    }
+
+    catch (err) {
+        return res.status(500).send('Internal Server Error: ' + err);
+    }
+});
+
+app.get('/user/getUser', async(req, res) => {
+    const { name } = req.query;
+
+    if (!name) {
+        return res.status(400).send('Name is required');
+    }
+
+    const params = {
+        TableName: 'Users',
+        Key: {
+            username: name,
+        }
+    };
+
+    try
+    {
+        const data = await dynamoDB.get(params).promise();
+
+        if (!data.Item) {
+            return res.status(404).send('User not found');
+        }
+
+        return res.send(data.Item);
+    }
+
+    catch (err) {
+        return res.status(500).send('Internal Server Error: ' + err);
+    }
+});
+
+app.delete('/user/deleteUser', async(req, res) => {
+    const { name } = req.query;
+
+    if (!name) {
+        return res.status(400).send('Name is required');
+    }
+
+    const params = {
+        TableName: 'Users',
+        Key: {
+            username: name,
+        }
+    };
+
+    try
+    {
+        const data = await dynamoDB.get(params).promise();
+
+        if (!data.Item) {
+            return res.status(404).send('User not found');
+        }
+
+        await dynamoDB.delete(params).promise();
+        return res.send('User deleted');
+    }
+
+    catch (err) {
+        return res.status(500).send('Internal Server Error: ' + err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = { app };
--- cdk/bin/hello-cdk.ts
+++ cdk/bin/hello-cdk.ts
@@ -57,7 +57,7 @@ new HelloCdkStack(app, 'HelloCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '079553702230', region: 'us-east-1' },
+  env: { account: '492027459158', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
