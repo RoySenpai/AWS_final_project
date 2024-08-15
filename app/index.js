bb20068ff0d1b241e904fb09c2478cacaffc0c16
Anna Pinchuk <76442355+annapinchuk@users.noreply.github.com>
2024-08-15 13:57:29 +0000

    Your commit message

--- app/index.js
+++ app/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
+const serverlessExpress = require('@vendia/serverless-express');
 
 AWS.config.update({ region: 'us-east-1' });
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: AWS_REGION });
-
-const port = 3000;
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 const app = express();
 app.use(express.json());
@@ -15,48 +14,38 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.post('/user/addUser', async(req, res) => {
-
+app.post('/user/addUser', async (req, res) => {
     const { name, email } = req.body;
 
     if (!name || !email) {
         return res.status(400).send('Name and Email are required');
     }
 
-    // Check if user already exists
     const params = {
         TableName: 'Users',
-        Key: {
-            username: name,
-        }
+        Key: { username: name },
     };
 
-    try
-    {
+    try {
         const data = await dynamoDB.get(params).promise();
 
         if (data.Item) {
             return res.status(400).send('User already exists');
         }
 
-        // The user does not exist, add the user, need to be a unique ID
-        const params = {
+        const putParams = {
             TableName: 'Users',
-            Item: {
-                username: name,
-                email: email,
-            }
+            Item: { username: name, email: email },
         };
 
-        await dynamoDB.put(params).promise();
-    }
-
-    catch (err) {
+        await dynamoDB.put(putParams).promise();
+        return res.send('User added');
+    } catch (err) {
         return res.status(500).send('Internal Server Error: ' + err);
     }
 });
 
-app.get('/user/getUser', async(req, res) => {
+app.get('/user/getUser', async (req, res) => {
     const { name } = req.query;
 
     if (!name) {
@@ -65,13 +54,10 @@ app.get('/user/getUser', async(req, res) => {
 
     const params = {
         TableName: 'Users',
-        Key: {
-            username: name,
-        }
+        Key: { username: name },
     };
 
-    try
-    {
+    try {
         const data = await dynamoDB.get(params).promise();
 
         if (!data.Item) {
@@ -79,14 +65,12 @@ app.get('/user/getUser', async(req, res) => {
         }
 
         return res.send(data.Item);
-    }
-
-    catch (err) {
+    } catch (err) {
         return res.status(500).send('Internal Server Error: ' + err);
     }
 });
 
-app.delete('/user/deleteUser', async(req, res) => {
+app.delete('/user/deleteUser', async (req, res) => {
     const { name } = req.query;
 
     if (!name) {
@@ -95,13 +79,10 @@ app.delete('/user/deleteUser', async(req, res) => {
 
     const params = {
         TableName: 'Users',
-        Key: {
-            username: name,
-        }
+        Key: { username: name },
     };
 
-    try
-    {
+    try {
         const data = await dynamoDB.get(params).promise();
 
         if (!data.Item) {
@@ -110,15 +91,9 @@ app.delete('/user/deleteUser', async(req, res) => {
 
         await dynamoDB.delete(params).promise();
         return res.send('User deleted');
-    }
-
-    catch (err) {
+    } catch (err) {
         return res.status(500).send('Internal Server Error: ' + err);
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = { app };
+module.exports.handler = serverlessExpress({ app });
